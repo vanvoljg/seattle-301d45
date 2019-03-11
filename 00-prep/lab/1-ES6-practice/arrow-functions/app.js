ad754ee6cf21a372baab1bf8c1469d74ac7444a7
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-11 11:43:40 -0700

    refactory objectLit to arrow function

--- 00-prep/lab/1-ES6-practice/arrow-functions/app.js
+++ 00-prep/lab/1-ES6-practice/arrow-functions/app.js
@@ -104,16 +104,9 @@ let sum = (a, b, c, d) => a + b + c + d;
 console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
-  return {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  };
-};
+let objectLit = () => ({key1: 'value1', key2: 'value2', key3: 'value3'});
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+console.log(objectLit());
 
 
 let sumAndProduct = function(a, b) {
